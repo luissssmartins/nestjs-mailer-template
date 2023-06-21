33b44a98a589815ff0cb72d6f068446832a5f3ff
Luis Martins <chateadorn@gmail.com>
2023-06-21 15:32:51 -0300

    rotas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Render, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
+
+  @Get('email')
+  @Render('form')
+  handleForm() {
+
+  }
+
+  @Post('send-email')
+  handleEmailProcessor(@Request() req) {
+    return this.appService.handleEmail(
+      req.body.email,
+      req.body.message,
+    );
+  }
 
   @Get()
   getHello(): string {
