39aaa0c4c387955a3dcd74fb9aacf645405268d5
Luis Martins <chateadorn@gmail.com>
2023-06-24 19:00:49 -0300

    up

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nest-modules/mailer';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class AppService {
+
+  constructor(private mailerService: MailerService) {}
+
+  async handleEmail(email: string, message: string) {
+    await this.mailerService.sendMail({
+
+      to: email,
+      from: process.env.SENDER,
+      subject: process.env.SUBJECT,
+      html: `<h3 style="color: red">${message}</h3>`
+
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
