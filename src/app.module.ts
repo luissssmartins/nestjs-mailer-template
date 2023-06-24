d118480f20a9468142c452fdf47df558d78ca561
Luis Martins <chateadorn@gmail.com>
2023-06-24 19:45:41 -0300

    simplificando classe do app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MailerModule } from '@nest-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+import { MailModule } from './modules/mail.module';
 
 @Module({
+  
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: false,
-        auth: {
-          user: process.env.MAILER_USER,
-          pass: process.env.MAILER_PASSWORD
-        },
-        ignoreTLS: true,
-      },
-      defaults: {
-        from: '"',
-      },
-    }),
+    ConfigModule.forRoot(),
+    MailModule
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
