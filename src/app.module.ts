fdd8855ae6096c1e17e2e7367de8ddff06474362
Luis Martins <chateadorn@gmail.com>
2023-06-24 19:05:12 -0300

    configurando modulo do email

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerModule } from '@nest-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: false,
+        auth: {
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASSWORD
+        },
+        ignoreTLS: true,
+      },
+      defaults: {
+        from: '"',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
